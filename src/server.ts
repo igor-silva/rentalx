b979f260dfc7d892a7f9357722a0b0267bda1c74
igor-silva <igor-d-silva@hotmail.com>
2024-01-02 23:16:25 -0300

    Removendo arquivo de upload e melhoria no swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 ﻿import express from 'express';
 import { router } from './routes';
+import fs from "fs";
+import path  from "path";
+import YAML from "yaml";
+import swaggerUi from "swagger-ui-express";
+import swaggerFileJson from "./swagger/swagger.json";
 
 const app = express();
+const swaggerPath = path.resolve(__dirname,'./swagger/swagger.yaml');
+const swaggerFile = fs.readFileSync(swaggerPath, "utf8");
+const swaggerDocument = YAML.parse(swaggerFile);
 
 app.use( express.json() );
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 
 app.listen(3000,
